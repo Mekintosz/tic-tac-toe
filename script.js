933f889fb5f5789c7b9f9e9d952ee003ced4243b
Mekintosz <michal.lazicki@gmail.com>
2023-08-16 16:59:45 +0200

    Player factory, gameBoard module and first game controler

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const gameBoard = (() => {
     let board = ["x", null, "o", "o", "x", "o", "x", "x", "o"];
+     const fields = document.querySelectorAll(".field");
+        
+        for (let i = 0; i < fields.length; i++) {
+            fields[i].innerText = `${board[i]}`
+        }
+        let getBoard = () => board;
+        let getFields = () => fields;
+    return {getBoard, getFields};
+
 })();
 
-const Player = (imie, pionek) => {
+const playerFactory = (imie, pionek) => {
     const getImie = () => imie;
     const getPionek = () => pionek;
     const lost = () => {
@@ -14,3 +23,15 @@ const Player = (imie, pionek) => {
     return {getImie, getPionek, lost, win}
 };
 
+const playerX = playerFactory("player1", "X")
+const playerY = playerFactory("player2", "O")
+
+const switchPlayers = () => {
+    currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+};
+
+const getCurrentPlayer = () => currentPlayer;
+
+
+
+const markField = (currentPlayer, getBoard, getFields)
