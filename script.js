c4ad5ac14a5a7a6310a21c95f1f6b1d207a301f1
Mekintosz <michal.lazicki@gmail.com>
2023-08-19 13:11:11 +0200

    gameControler function

--- script.js
+++ script.js
@@ -1,37 +1,46 @@
-const gameBoard = (() => {
-    let board = ["x", null, "o", "o", "x", "o", "x", "x", "o"];
-     const fields = document.querySelectorAll(".field");
+const gameBoard = () => {
+
+    let board = ["x", null, "o",
+                 "o", "x", "o", 
+                 "x", "x", "o"];
+
+    let getBoard = () => board;
+
+    let markField = function(player, field) {
+        board[field] = player.token
+    }
+
+    return getBoard, markField;
+};
+
+
+function gameControler() {
+
+    const playerX = {
+        name = "player1",
+        token = "x"
+    }
+
+    const playerO = {
+        name = "player2",
+        token = "o"
+    }
+
+    let currentPlayer = playerX
+    const switchPlayers = () => {
+        currentPlayer = currentPlayer === playerX ? playerO : playerX;
+    };
+
+    const getCurrentPlayer = () => currentPlayer;
+
+    const game
+
+    return getCurrentPlayer, switchPlayers
+}
+
+const fields = document.querySelectorAll(".field");
         
+     const setBoard = function(fields){
         for (let i = 0; i < fields.length; i++) {
             fields[i].innerText = `${board[i]}`
         }
-        let getBoard = () => board;
-        let getFields = () => fields;
-    return {getBoard, getFields};
-
-})();
-
-const playerFactory = (imie, pionek) => {
-    const getImie = () => imie;
-    const getPionek = () => pionek;
-    const lost = () => {
-        // something happens when lost
-    };
-    const win = () => {
-        //when win
-    };
-    return {getImie, getPionek, lost, win}
-};
-
-const playerX = playerFactory("player1", "X")
-const playerY = playerFactory("player2", "O")
-
-const switchPlayers = () => {
-    currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
-};
-
-const getCurrentPlayer = () => currentPlayer;
-
-
-
-const markField = (currentPlayer, getBoard, getFields)
