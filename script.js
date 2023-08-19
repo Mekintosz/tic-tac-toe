339f4c7182e007ed4cc37bdee90c3351988a1f89
Mekintosz <michal.lazicki@gmail.com>
2023-08-19 15:59:31 +0200

    update DisplayController, buttons for null fields

--- script.js
+++ script.js
@@ -1,46 +1,65 @@
-const gameBoard = () => {
+const gameBoard = (() => {
 
     let board = ["x", null, "o",
                  "o", "x", "o", 
                  "x", "x", "o"];
 
-    let getBoard = () => board;
+    const getBoard = () => board;
 
-    let markField = function(player, field) {
-        board[field] = player.token
-    }
+    const setBoard = function (fields){
+        for (let i = 0; i < fields.length; i++) {
+            fields[i].innerText = `${board[i]}`
+        }
+    };
 
-    return getBoard, markField;
-};
+    return {getBoard, setBoard}
+})();
 
+const Player = (name, token) => {
+    const getName = () => name;
+    const getToken = () => token;
 
-function gameControler() {
+    return {getName, getToken}
+}
 
-    const playerX = {
-        name = "player1",
-        token = "x"
-    }
+const gameController = () =>  {
 
-    const playerO = {
-        name = "player2",
-        token = "o"
-    }
-
+    const playerX = Player("player1", "x")
+    const playerO = Player("player2", "o")
+    
     let currentPlayer = playerX
+
     const switchPlayers = () => {
         currentPlayer = currentPlayer === playerX ? playerO : playerX;
     };
 
     const getCurrentPlayer = () => currentPlayer;
 
-    const game
+    let markField = function(player, field) {
+        gameBoard.getBoard[field] = player.token
+    }
 
-    return getCurrentPlayer, switchPlayers
+    return {getCurrentPlayer, switchPlayers}
 }
 
-const fields = document.querySelectorAll(".field");
+const displayController = (() => {
+    const game = gameController();
+    const board = gameBoard;
+    const boardSet = gameBoard.getBoard();
+    const fields = document.querySelectorAll(".field");
+    board.setBoard(fields);
+
+    const activePlayer = game.getCurrentPlayer();
+
+    boardSet.forEach( (field, index) => {
+    
+        if (field === null){
+            const divButton = document.createElement("button");
+            divButton.classList.add("field-button");
+            const div = document.getElementById(`${index}`)
+            div.appendChild(divButton);
+        } else return
         
-     const setBoard = function(fields){
-        for (let i = 0; i < fields.length; i++) {
-            fields[i].innerText = `${board[i]}`
-        }
+    })
+})();
+
