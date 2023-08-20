6fb87b70d3fe7a9d1145526578c4a9c412d64abe
Mekintosz <michal.lazicki@gmail.com>
2023-08-20 14:21:02 +0200

    Applied new logic and styling

--- script.js
+++ script.js
@@ -1,65 +1,86 @@
 const gameBoard = (() => {
 
-    let board = ["x", null, "o",
+    let start = ["x", null, "o",
                  "o", "x", "o", 
                  "x", "x", "o"];
 
-    const getBoard = () => board;
+    let move = "cross"
+    const getBoard = () => start;
 
-    const setBoard = function (fields){
-        for (let i = 0; i < fields.length; i++) {
-            fields[i].innerText = `${board[i]}`
-        }
+    const board = document.querySelector("#board")
+    const display = document.querySelector("#info")
+    display.textContent = "Cross goes first."
+    const setBoard = function (){
+        start.forEach((_cell, index) => {
+        const cellElement = document.createElement("div")
+        cellElement.classList.add("square")
+        cellElement.id = index
+        cellElement.addEventListener("click", addMove)
+        board.append(cellElement)
+
+        })
     };
 
+    function addMove(e) {
+        moveDisplay = document.createElement("div")
+        moveDisplay.classList.add(move)
+        e.target.append(moveDisplay)
+        whoseMove()
+        display.textContent = "it is now" + move + "'s move"
+        e.target.removeEventListener("click", addMove)
+    }
+
+    whoseMove = () => {move = move === "cross" ? "circle" : "cross"}
     return {getBoard, setBoard}
 })();
 
-const Player = (name, token) => {
-    const getName = () => name;
-    const getToken = () => token;
+gameBoard.setBoard();
 
-    return {getName, getToken}
-}
+// const Player = (name, token) => {
+//     const getName = () => name;
+//     const getToken = () => token;
 
-const gameController = () =>  {
+//     return {getName, getToken}
+// }
 
-    const playerX = Player("player1", "x")
-    const playerO = Player("player2", "o")
+// const gameController = () =>  {
+
+//     const playerX = Player("player1", "x")
+//     const playerO = Player("player2", "o")
     
-    let currentPlayer = playerX
+//     let currentPlayer = playerX
 
-    const switchPlayers = () => {
-        currentPlayer = currentPlayer === playerX ? playerO : playerX;
-    };
+//     const switchPlayers = () => {
+//         currentPlayer = currentPlayer === playerX ? playerO : playerX;
+//     };
 
-    const getCurrentPlayer = () => currentPlayer;
+//     const getCurrentPlayer = () => currentPlayer;
 
-    let markField = function(player, field) {
-        gameBoard.getBoard[field] = player.token
-    }
+//     let markField = function(player, field) {
+//         gameBoard.getBoard[field] = player.token
+//     }
 
-    return {getCurrentPlayer, switchPlayers}
-}
+//     return {getCurrentPlayer, switchPlayers}
+// }
 
-const displayController = (() => {
-    const game = gameController();
-    const board = gameBoard;
-    const boardSet = gameBoard.getBoard();
-    const fields = document.querySelectorAll(".field");
-    board.setBoard(fields);
+// const displayController = (() => {
+//     const game = gameController();
+//     const board = gameBoard;
+//     const boardSet = gameBoard.getBoard();
+//     const fields = document.querySelectorAll(".field");
+//     board.setBoard(fields);
 
-    const activePlayer = game.getCurrentPlayer();
+//     const activePlayer = game.getCurrentPlayer();
 
-    boardSet.forEach( (field, index) => {
+//     boardSet.forEach( (field, index) => {
     
-        if (field === null){
-            const divButton = document.createElement("button");
-            divButton.classList.add("field-button");
-            const div = document.getElementById(`${index}`)
-            div.appendChild(divButton);
-        } else return
+//         if (field === null){
+//             const divButton = document.createElement("button");
+//             divButton.classList.add("field-button");
+//             const div = document.getElementById(`${index}`)
+//             div.appendChild(divButton);
+//         } else return
         
-    })
-})();
+//     })
+// })();
 
